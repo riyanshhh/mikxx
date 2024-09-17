7beaf1dbb3d4e7b886222398b982bd5a2671aa1e
riyanshhh <ahlawat.riyansh@gmail.com>
2024-09-17 22:38:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDhz3wB1uNVEpln-nfZ3XMNLu4NMhZ_Xx0",
+  authDomain: "mikxx-193ec.firebaseapp.com",
+  databaseURL: "https://mikxx-193ec-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mikxx-193ec",
+  storageBucket: "mikxx-193ec.appspot.com",
+  messagingSenderId: "928424108366",
+  appId: "1:928424108366:web:3d1755ae4221a74d673e1f",
 };
 
 // Initialize Firebase
