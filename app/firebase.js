86bbb9a0b526f72b111188f273ee0679818ee5f4
riyanshhh <ahlawat.riyansh@gmail.com>
2024-09-19 14:28:32 +0530

    1

--- app/firebase.js
+++ app/firebase.js
@@ -1,19 +1,19 @@
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDhz3wB1uNVEpln-nfZ3XMNLu4NMhZ_Xx0",
-    authDomain: "mikxx-193ec.firebaseapp.com",
-    databaseURL: "https://mikxx-193ec-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "mikxx-193ec",
-    storageBucket: "mikxx-193ec.appspot.com",
-    messagingSenderId: "928424108366",
-    appId: "1:928424108366:web:3d1755ae4221a74d673e1f"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // Function to send signaling data
-function sendSignal(signal) {
+export function sendSignal(signal) {
     console.log("Sending signal:", signal);
     database.ref('signals').push(signal);
 }
@@ -29,3 +29,6 @@ function handleSignal(signal) {
     console.log("Received signal:", signal);
     // Handle the signal as before
 }
+
+// Export the database variable
+export { database }; // Add this line to export the database variable
